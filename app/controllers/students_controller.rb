075983b84938b1b2770e6016cad2a7bfa6818e5b
csherpa <chhoti0712@gmail.com>
2018-08-28 12:54:56 -0400

    made change in create method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,46 +1,36 @@
 class StudentsController < ApplicationController
-    class StudentsController < ApplicationController
-
-        def index
-            @students = Student.all
-        end
-
-        def new
-            @student = Student.new
-        end
-    
-        def show
-            @student = Student.find(params[:id])
-        end
-        # def create
-        #     Student.create(student_params)
-        #     redirect_to new_student_path
-        # end
-    
-        def create 
-            @student = Student.new(student_params)
-            if @student.save 
-                redirect_to '/students' 
-            else 
-                render 'new' 
-            end 
-        end
-    
-        def edit 
-            @student = Student.find(params[:id]) 
-        end
-          
-        def update
-            @student = Student.find(params[:id])
-            @student.update(student_params)
-            redirect_to '/students'
-        end
-    
-        private
-    
-        def student_params
-            params.require(:student).permit(:firstname, :lastname, :cohort_id)
-        end
+    def index
+        @students = Student.all
     end
-    
+
+
+    def new
+        @student = Student.new
+    end
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def create 
+        @student = Student.new(student_params)
+        @student.save 
+        redirect_to '/students' 
+    end
+
+    def edit 
+        @student = Student.find(params[:id]) 
+    end
+      
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params)
+        redirect_to '/students'
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:firstname, :lastname, :cohort_id)
+    end
 end
