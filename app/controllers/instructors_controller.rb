ad80edd0af3885eca8738eb1f3da8f72335fb834
csherpa <chhoti0712@gmail.com>
2018-08-29 17:03:51 -0400

    modified the instructors view files

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -7,30 +7,30 @@ def new
     @instructor = Instructor.new
 end
 
-def create
-    @instructor = Instructor.new(instructor_params)
-    @instructor.save
-    redirect_to '/instructors'
-end
-
 def show
     @instructor = Instructor.find(params[:id])
 end 
 
 def edit 
     @instructor = Instructor.find(params[:id]) 
-  end
+end
   
-  def update
+def create
+    @instructor = Instructor.new(instructor_params)
+    @instructor.save
+    redirect_to '/instructors'
+end
+
+def update
     @instructor = Instructor.find(params[:id])
     @instructor.update(instructor_params)
     redirect_to '/instructors'
 end
 
 def destroy
-    @instructor = Student.find(params[:id])
-    @instructor.delete
-    redirect_to '/students'
+    @instructor = Instructor.find(params[:id])
+    @instructor.destroy
+    redirect_to '/instructors'
 end
 
 private
