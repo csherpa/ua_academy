f925cd5526e0f1af8ee77c64b29131cc17fffd2a
csherpa <chhoti0712@gmail.com>
2018-08-31 23:53:07 -0400

    made ajax call work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
 let studentdiv;
+let instructordiv;
 let form;
--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -7,17 +7,15 @@ class CohortsController < ApplicationController
         @cohort = Cohort.new
     end
 
-    def create 
-        @cohort = Cohort.new(cohort_params)
-        if @cohort.save
-            redirect_to '/cohorts'
-        else
-            render 'new'
-        end
-    end
-
     def show
         @cohort = Cohort.find(params[:id])
+    end
+    
+    def create 
+        @cohort = Cohort.new(cohort_params)
+        @cohort.save
+        redirect_to '/cohorts'
+       
     end
 
     def edit 
